a682db90a47abe58599c899e875e5924020b904e
RonieDevStitch <rssungjae1@gmail.com>
2022-04-01 05:40:42 +0900

    [add] JavaScript-OOP
    for study of basic javascript
    the dreamcode

---
+++ JavaScript-OOP/objectVsClass.js
@@ -0,0 +1,104 @@
+// https://www.youtube.com/watch?v=_DLhUBWsRtw
+
+// 1. Class declarations
+class Person{
+    // constructor
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    // methods
+    speak(){
+        console.log(`${this.name} : hello!`);
+    }
+}
+
+const ellie = new Person('ellie', 20);
+console.log(ellie.name);
+console.log(ellie.age);
+ellie.speak();
+
+// 2. Getter and setters
+class User{
+    constructor(firstName, lastName, age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    get age(){
+        return this._age;
+    }
+
+    set age(value){
+        this._age = value < 0 ? 0 : value;
+    }
+
+}
+
+const user1 = new User('Steve', 'Job', -1);
+console.log(user1.age);
+
+// 3. Fields(public, private)
+class Experiment{
+    publicField = 3;
+    #privateField = 0;
+}
+const experiment = new Experiment();
+console.log(experiment.publicField);
+console.log(experiment.privateField);
+
+// 4. Static properties and methods
+class Article {
+    static publisher = 'Dream Coding';
+    constructor(articleNumber){
+        this.articleNumber = articleNumber;
+    }
+
+    static printPublisher(){
+        console.log(Article.publisher);
+    }
+}
+
+const article1 = new Article(1);
+const article2 = new Article(2);
+console.log(article1.publisher);
+console.log(Article.publisher);
+
+// 5. Inheritance
+class Shape{
+    constructor(width, height, color){
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    draw(){
+        console.log(`drawing ${this.color} color!`);
+    }
+
+    getArea(){
+        return this.width * this.height;
+    }
+}
+
+class Rectangle extends Shape{}
+class Triangle extends Shape{
+    getArea(){
+        return (super.getArea())/2;
+    }
+}
+
+const rectangle = new Rectangle(20,20,'blue');
+rectangle.draw();
+console.log(rectangle.getArea());
+const triangle = new Triangle(20,20,'red');
+triangle.draw();
+console.log(triangle.getArea());
+
+// 6. Class checking: instanceOf
+console.log(rectangle instanceof Rectangle);
+console.log(triangle instanceof Rectangle);
+console.log(triangle instanceof Triangle);
+console.log(triangle instanceof Shape);
+console.log(triangle instanceof Object);
